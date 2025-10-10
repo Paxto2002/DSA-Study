2948e20da5f0c84aef4f2e4aeed92a84fe0963b3
paxto2002 <hafizwaseemahmed2002@gmail.com>
2025-10-10 21:45:07 +0500

    Understood Prime Number Function

---
+++ 2_loops_questions_and_patterns/3_prime_checker.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+int main()
+{
+    int n;
+    bool isPrime = true;
+    std::cout << "Enter the value for n: ";
+    std::cin >> n;
+
+    if (n <= 1)
+    {
+        isPrime = false;
+    }
+    else
+    {
+
+        for (int i = 2; i * i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+    if (isPrime)
+    {
+        std::cout << n << " is a prime number" << std::endl;
+    }
+    else
+    {
+        std::cout << n << " is not a prime number" << std::endl;
+    }
+    return 0;
+}
